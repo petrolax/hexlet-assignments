a685565b2ffa0633ffb44fb3b94bfdb0730ba3a4
@hexlet/cli <support@hexlet.io>
2024-06-26 08:21:30 +0300

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+require 'uri'
+require 'forwardable'
+# BEGIN
+class Url
+  attr_accessor :url
+  extend Forwardable
+  def initialize(str)
+    @url = URI(str)
+    @qparams = {}
+  end
+  def_delegators :@url, :scheme, :host, :port
+
+  def query_params
+    @qparams = url.query.nil? ? {} : URI.decode_www_form(url.query).to_h.transform_keys(&:to_sym)
+    @qparams
+  end
+
+  def query_param(key, val = nil)
+    self.query_params if @qparams.nil? || @qparams.empty?
+    res = @qparams[key]
+    return val if res.nil?
+    res
+  end
+
+  include Comparable
+  def ==(other)
+    return false  if self.scheme != other.scheme
+    return false  if self.host != other.host
+    return false  if self.port != other.port
+    return false if self.query_params.length != other.query_params.length
+
+    self.query_params.reduce(true) do |res, (k,v)|
+      res ||= true
+      val = other.query_param(k)
+      break false if val.nil?
+      break false if v != val
+      res
+    end
+  end
+end
+
+# u = Url.new 'https://google.com?key=value&key2=value'
+# puts u.scheme
+# puts u.host
+# puts u.port
+
+# # puts u.query_params
+# # puts u.query_params == {key: 'value'}
+
+# puts u.query_param(:val).nil?
+# puts u.query_param(:new, 'ehu') == 'ehu'
+
+# ur = Url.new 'https://google.com?key2=value&key=valu'
+
+# puts u == ur
+
+# END
